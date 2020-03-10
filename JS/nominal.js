429078fa39857ef7bde7f7dd395d1fadfd71af0d
Wil-Rs <wilianrsantos.pro@gmail.com>
2020-03-09 23:31:48 -0500

    falta so continua e verificar umas coisa

--- JS/nominal.js
+++ JS/nominal.js
@@ -15,7 +15,47 @@ function contaVetor(vetor){
 }
 // APENAS PARA LEMBRAR DE COMO CONTAR NAO MEXER
 
-function tabela(nomeVariavelTabela, localDaTabelaSite){
+//--------------------------------------------------------------
+const tipoCalculo = document.getElementsByName('calculoSelecionado')
+const mostraDiv = document.querySelector('#ordemVer')
+const tirarTabela = document.querySelector(`#calculoNominal`)
+
+if (tipoCalculo[1].checked) mostraDiv.style.display = 'block'
+tipoCalculo[1].onchange = e => {
+	if( e.isTrusted ){
+		mostraDiv.style.display = 'block'
+		tirarTabela.style.display = 'none'
+	}
+}
+
+tipoCalculo[0].onchange = e => {
+	if( e.isTrusted ){
+		mostraDiv.style.display = 'none'
+		tirarTabela.style.display = 'none'
+	}
+}
+
+tipoCalculo[2].onchange = e => {
+	if( e.isTrusted ){
+		mostraDiv.style.display = 'none'
+		tirarTabela.style.display = 'none'
+	}
+}
+
+tipoCalculo[3].onchange = e => {
+	if( e.isTrusted ){
+		mostraDiv.style.display = 'none'
+		tirarTabela.style.display = 'none'
+	}
+}
+
+//--------------------------------------------------------------
+// const localDaTabela = document.querySelector(`#$calculoNominal`)
+// tirarTabela.style.display = 'none'
+
+function nominal(nomeVariavelTabela, localDaTabelaSite){
+	const tipoCalculo = document.getElementsByName('calculoSelecionado')
+
 	const localDaTabela = document.querySelector(`#${localDaTabelaSite}`)
 	const NomeTabela = document.querySelector('#variavelNome')
 	const nomeVariavel = document.querySelector('#nomeVariavel')
@@ -39,15 +79,68 @@ function tabela(nomeVariavelTabela, localDaTabelaSite){
 		return obj
 	}, {} )
 
-	// ADD OS VALORES NA TABELA
-	const corpoTabela = document.querySelector('#corpo')
-	corpoTabela.innerHTML = ``
-	cont = 0 
-	Object.keys(sep).forEach( item => {
-		corpoTabela.innerHTML += `<tr> <td>${item}</td> <td>${sep[item]}</td> </tr>`
-		cont += sep[item]
-	})
-	corpoTabela.innerHTML += `<tr> <td id="total">Total</td> <td id="total">${cont}</td> </tr>`
+	if( tipoCalculo[0].checked ){
+		// ADD OS VALORES NA TABELA NOMINAL
+		const corpoTabela = document.querySelector('#corpo')
+		corpoTabela.innerHTML = ``
+		cont = 0 
+		Object.keys(sep).forEach( item => {
+			corpoTabela.innerHTML += `<tr> <td>${item}</td> <td>${sep[item]}</td> </tr>`
+			cont += sep[item]
+		})
+		corpoTabela.innerHTML += `<tr> <td id="total">Total</td> <td id="total">${cont}</td> </tr>`
+		// FIM TABELA NOMINAL
+
+	}else if( tipoCalculo[1].checked ){
+		// ADD OS VALORES NA TABELA ORDINAL
+		let ordemInput = document.querySelector('#ordem').value.split(',')
+		ordemInput = ordemInput.filter( (este, i) => ordemInput.indexOf(este) === i )
+
+		const corpoTabela = document.querySelector('#corpo')
+		corpoTabela.innerHTML = ``
+		cont = 0 
+		ordemInput.forEach( item => {
+			corpoTabela.innerHTML += `<tr> <td>${item}</td> <td>${sep[item]}</td> </tr>`
+			cont += sep[item]
+		})
+		corpoTabela.innerHTML += `<tr> <td id="total">Total</td> <td id="total">${cont}</td> </tr>`
+		// FIM TABELA ORDINAL
+
+	}else if( tipoCalculo[2].checked ){
+		// ADD OS VALORES NA TABELA DISCRETA
+		const corpoTabela = document.querySelector('#corpo')
+		corpoTabela.innerHTML = ``
+		cont = 0 
+		Object.keys(sep).forEach( item => {
+			corpoTabela.innerHTML += `<tr> <td>${item}</td> <td>${sep[item]}</td> </tr>`
+			cont += sep[item]
+		})
+		corpoTabela.innerHTML += `<tr> <td id="total">Total</td> <td id="total">${cont}</td> </tr>`
+		// FIM TABELA NOMINAL
+		alert('Verificar com a prof pois essa nao foi entendida')
+		// FIM TABELA DISCRETA
+
+	}else if( tipoCalculo[3].checked ){
+		// ADD OS VALORES NA TABELA CONTINUA
+		
+		// 1°passo
+		let min = Number( dados[0] )
+		let max = Number( dados[0] )
+		
+		dados.forEach( item => {
+			if( Number(item) > max ) max = item
+			if( Number(item) < min ) min = item
+		} )
+		let at = max - min
+
+		// 2°passo
+		let k = Math.sqrt( dados.length ) // raiz quadrada do total dos elementos
+		console.log(`kkkk ${k} ${k - 1} ${k + 1}`)
+
+		// FIM TABELA CONTINUA
+	}
+
+
 }
 
 
@@ -61,18 +154,20 @@ document.querySelector('#BotaoCalcular').onclick = e => {
 	// const dadosVariavel = document.querySelector('#dadosVariavel')
 	const tipoCalculo = document.getElementsByName('calculoSelecionado')
 
-	if( tipoCalculo[0].checked ){
-		tabela('nomeVariavel', 'calculoNominal')
-		// alert('Qualitativa Nominal')
+	nominal('nomeVariavel', 'calculoNominal')
 
-	}else if( tipoCalculo[1].checked ){
-		alert(' Qualitativa Ordinal ')
+	// if( tipoCalculo[0].checked ){
+	// 	nominal('nomeVariavel', 'calculoNominal')
+	// 	// alert('Qualitativa Nominal')
 
-	}else if( tipoCalculo[2].checked ){
-		alert(' Quantitativa Discreta ')
+	// }else if( tipoCalculo[1].checked ){
+	// 	alert(' Qualitativa Ordinal ')
 
-	}else if( tipoCalculo[3].checked ){
-		alert('Quantitativa Continua ')
+	// }else if( tipoCalculo[2].checked ){
+	// 	alert(' Quantitativa Discreta ')
 
-	}
+	// }else if( tipoCalculo[3].checked ){
+	// 	alert('Quantitativa Continua ')
+
+	// }
 }
