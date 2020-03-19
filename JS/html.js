ecba0ecd966d16657970987728133934d6fc843e
Gabriel <gabrielribeiro1698@gmail.com>
2020-03-19 20:38:52 -0300

    Inserindo o Botão de Upload

---
+++ JS/html.js
@@ -0,0 +1,17 @@
+const btnUpload = document.getElementById("botaoUpload");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  btnUpload.click();
+});
+
+btnUpload.addEventListener("change", function() {
+  if (btnUpload.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "Nenhum arquivo selecionado";
+  }
+});
