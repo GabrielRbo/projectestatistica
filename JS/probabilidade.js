c8fec53c19c0980a1195ca13763cd6bc5e28a2d0
Gabriel Ribeiro <gabrielribeiro1698@gmail.com>
2020-06-21 15:12:21 -0300

    Zerando campos ao selecionar outro calculo

--- JS/probabilidade.js
+++ JS/probabilidade.js
@@ -7,7 +7,9 @@ $(opCalc[0]).click(function () {
     $('#calcBinomial.escondido').css("display","block")
     
     document.getElementById('calcNormal').style.display = 'none'
+    $('#calcNormal :input').val('');
     document.getElementById('calcUniforme').style.display = 'none'
+    $('#calcUniforme :input').val('');
     
 })
 
@@ -15,12 +17,16 @@ $(opCalc[1]).click(function () {
     $('#calcNormal.escondido').css("display","block")
 
     document.getElementById('calcBinomial').style.display = 'none'
+    $('#calcBinomial :input').val('');
     document.getElementById('calcUniforme').style.display = 'none'
+    $('#calcUniforme :input').val('');
 })
 
 $(opCalc[2]).click(function () {
     $('#calcUniforme.escondido').css("display","block")
 
     document.getElementById('calcNormal').style.display = 'none'
+    $('#calcNormal :input').val('');
     document.getElementById('calcBinomial').style.display = 'none'
+    $('#calcBinomial :input').val('');
 })
