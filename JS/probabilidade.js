aa09a635fcd2487dc8bd707a08659ddf51a1f8f6
Wil-Rs <wilianrsantos.pro@gmail.com>
2020-06-21 21:40:11 -0300

    prob

--- JS/probabilidade.js
+++ JS/probabilidade.js
@@ -29,4 +29,58 @@ $(opCalc[2]).click(function () {
     $('#calcNormal :input').val('');
     document.getElementById('calcBinomial').style.display = 'none'
     $('#calcBinomial :input').val('');
-})
+}) 
+
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+const btBinomial = document.querySelector('#btCalcBino')
+
+function fatorial(n){
+    let soma = 1
+    for (i = 1; i <= n; i++){
+        soma *= i
+    }
+    return soma
+}
+
+function prob (n, k){
+    return fatorial(n) / ( fatorial(n - k) * fatorial(k) )
+}
+
+function media( n, p ){
+    return n * p
+}
+
+function desvioPadra (n, p, q){
+    return Math.sqrt( n * p * q )
+}
+
+btBinomial.onclick = () => {
+    const tabP = document.querySelector('#tabProb')
+    const tabM = document.querySelector('#tabMedia')
+    const tabD = document.querySelector('#tabDesvio')
+
+    const eleN = document.querySelector('#elementoN')
+    const eleP = document.querySelector('#elementoP')
+    const eleQ = document.querySelector('#elementoQ')
+    const eleK = document.querySelector('#elementoK')
+
+    let n = eleN.value
+    let p = eleP.value
+    let q = eleQ.value
+    let k = eleK.value
+
+    tabP.innerHTML = prob(n, k)
+    tabM.innerHTML = media(n, p).toFixed(2)
+    tabD.innerHTML = desvioPadra(n, p, q)
+
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+const btNormal = document.querySelector('#btCalcNormal')
+const btUniform = document.querySelector('#btCalcUniform')
