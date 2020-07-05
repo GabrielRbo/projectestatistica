c649b0eebc9dfd58c2fa779eca4a97d19e614e79
Wil-Rs <wilianrsantos.pro@gmail.com>
2020-07-04 21:25:39 -0300

    correlacao

--- JS/correlacao.js
+++ JS/correlacao.js
@@ -12,6 +12,31 @@ const pontoF = document.querySelector('#pontoF')
 let resultadoRegressao = 0
 let fA = 0
 let fB = 0
+
+
+function encontrarPosMenor(vetor, posIni){
+    let posMenor = posIni
+    for (let i = posIni + 1; i < vetor.length; i++){
+        if(vetor[i] < vetor[posMenor]) posMenor = i
+    }
+    return posMenor
+}
+
+function troca(vetor, i, j){
+    let aux = vetor[i]
+    vetor[i] = vetor[j]
+    vetor[j] = aux
+}
+
+function selectionSort(vetor){
+    for(let i = 0;  i < vetor.length - 1; i++){
+        let posMenor = encontrarPosMenor(vetor, i + 1)
+        if (vetor[posMenor] < vetor[i]) {
+            troca(vetor, posMenor, i)
+        }
+    }
+}
+
 function grafico(dados, dadosG){
 
     var ctx = document.getElementById('gf').getContext('2d');
@@ -63,8 +88,6 @@ btR.onclick = () => {
 		x2.push( i**2 )
 		somaX2 += i**2
 	}
-	console.log('SomaX ' + somaX)
-	console.log('x2 ' + x2)
 	
 	for( i of Vy){
 		somaY += Number(i)
@@ -78,18 +101,7 @@ btR.onclick = () => {
 		xy.push( Vx[i] * Vy[i] )
 		somaXY += Vx[i] * Vy[i]
 		dadosGrafico.push( {x: Vx[i] ,y: Vy[i] } )
-		if ( maior < Vy[i] ){
-			maior = Vy[i]
-		}
-		if( menor > Vy[i] ){
-			menor = Vy[i]
-		}
 	}
-	console.log('SomaX ' + somaX)
-	console.log('SomaY ' + somaY)
-	console.log('somaX2 ' + somaX2)
-	console.log('somaY2 ' + somaY2)
-	console.log('somaXY '+ somaXY)
 
 	//Correlacao
 	a = Vx.length * somaXY - somaX * somaY
@@ -101,17 +113,14 @@ btR.onclick = () => {
 	// aB1 = (Vx.length * somaXY - somaX * somaY)
 	// aB2 = (Vx.length * somaX2 - somaX ** 2)
 	B = (Vx.length * somaXY - somaX * somaY) / (Vx.length * somaX2 - somaX ** 2)
-	console.log(B)
 
 	A = (somaY - B * somaX) / Vx.length
-	console.log('a' + A)
 	ar = A.toFixed(3)
 	br = B.toFixed(4)
-	if( br.indexOf('-') ){
-		br = br.slice(0, )
+	if( Number(br) < 0){
+		br = Number(br) * -1
 	}
 	reta = `${ar} + ${br} * x`
-	console.log('reta ' + reta)
 
 	//add tabela correlacao e regrecao
 	rCorrelacao.innerHTML = resultado.toFixed(2)
@@ -119,12 +128,25 @@ btR.onclick = () => {
 	// resultadoRegressao = `${A.toFixed(3)} + ${B.toFixed(4)} * x`
 	fA = A.toFixed(3)
 	fB = B.toFixed(4)
+	if( Number(fB) < 0){
+		fB = Number(fB) * -1
+	}
+	
+	auxData = []
+	for ( i of Vy){
+		auxData.push( Number(i) )
+	}
+	
+	selectionSort(auxData)
+
+	menor = auxData[0]
+	maior = auxData.slice(-1)
+
 	dataG = [{
         x:(menor - A ) / B , y: menor
     },{
         x:(maior - A ) / B , y: maior
     }]
-
 
 	grafico(dadosGrafico, dataG)
 }
@@ -134,11 +156,9 @@ bt.onclick = () => {
 	pontof = []
 	if( nVx.value != "" ){
 		resultadoRegressao = Number(fA) + Number(fB) * Number(nVx.value)
-		console.log('x' + resultadoRegressao)
 		pontof.push( {x:nVx.value, y:resultadoRegressao} )
 	}else if( nVy.value != "" ){
 		resultadoRegressao = ((nVy.value - fA) / fB).toFixed(0)
-		console.log('x' + resultadoRegressao)
 		pontof.push( {x:resultadoRegressao, y:nVy.value} )
 
 	}
